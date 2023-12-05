8eaa0d199b538d625de541b548ee732e7d5a587e
tirth4112 <tdpanchal4112@gmail.com>
2023-12-05 19:39:47 +0530

    cop

--- subscribe-services/auth-consumer-services/Subscriber/login-subscribe.js
+++ subscribe-services/auth-consumer-services/Subscriber/login-subscribe.js
@@ -1,42 +1,42 @@
 const amqp = require('amqplib');
-const addUserToMongoDB=require('../controller/login-subscribe')
+const addUserToMongoDB = require('../controller/login-subscribe');
+
 async function consumeQueueAndPostToDatabase() {
-    try {
-      // Connect to RabbitMQ
-      const connection = await amqp.connect('amqp://localhost');
-      const channel = await connection.createChannel();
-  
-      // Assert the existence of the queue
-      await channel.assertQueue('auth_queue', { durable: false });
-  
-      // Consume messages from the queue
-      channel.consume('auth_queue', async (message) => {
-        if (message !== null) {
-          const data = message.content.toString();
-          console.log('Received message from queue:', data);
-  
+  try {
+    // Connect to RabbitMQ
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    // Assert the existence of the queue
+    await channel.assertQueue('auth_queue', { durable: false });
+
+    // Consume messages from the queue
+    channel.consume('auth_queue', async (message) => {
+      if (message !== null) {
+        const data = message.content.toString();
+        console.log('Received message from queue:', data);
+
+        // Check if the message content is not empty
+        if (data.trim() !== '') {
           // Parse the data from the message
           try {
             const { username } = JSON.parse(data);
-  
+
             // Insert the user data into MongoDB
             await addUserToMongoDB(username);
-            // console.log('User added to MongoDB successfully.');
-  
-            // Acknowledge the message to remove it from the queue
             channel.ack(message);
           } catch (parseError) {
             console.error('Error parsing message data:', parseError.message);
           }
+        } else {
+          console.error('Received empty message from the queue.......');
         }
-      });
-    } catch (error) {
-      console.error('Error consuming queue and posting to database:', error.message);
-      throw error;
-    }
+      }
+    });
+  } catch (error) {
+    console.error('Error consuming queue and posting to database:', error.message);
+    throw error;
   }
-
-
-  
+}
 
 module.exports = consumeQueueAndPostToDatabase;
--- subscribe-services/auth-consumer-services/app.js
+++ subscribe-services/auth-consumer-services/app.js
@@ -9,3 +9,54 @@ consumeQueueAndPostToDatabase();
 app.listen(port, () => {
   console.log(`Auth subscribe service is running on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // loadBalancer.js
+// const { fork } = require('child_process');
+
+// const MIN_SUBSCRIBERS = 1; // Minimum number of subscribers
+// const MAX_SUBSCRIBERS = 5; // Maximum number of subscribers
+
+// const subscribers = [];
+
+// function scaleUp() {
+//   const newSubscriber = fork('subscriber.js'); // Start a new subscriber instance
+//   subscribers.push(newSubscriber);
+//   console.log('Scaled up. Total subscribers:', subscribers.length);
+// }
+
+// function scaleDown() {
+//   const lastSubscriber = subscribers.pop();
+//   lastSubscriber.kill(); // Terminate the last subscriber instance
+//   console.log('Scaled down. Total subscribers:', subscribers.length);
+// }
+
+// // Implement your dynamic scaling policy here (e.g., based on message backlog)
+
+// // Example: Scale up if message backlog is high
+// const messageBacklog = 100;
+// if (messageBacklog > 50 && subscribers.length < MAX_SUBSCRIBERS) {
+//   scaleUp();
+// }
+
+// // Example: Scale down if message backlog is low
+// if (messageBacklog < 10 && subscribers.length > MIN_SUBSCRIBERS) {
+//   scaleDown();
+// }
