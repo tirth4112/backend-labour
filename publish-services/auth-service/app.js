6570578f76b9708b8a027c184322c32c46e637fd
tirth_dell <tdpanchal4112@gmail.com>
2023-12-16 22:10:06 +0530

    changes in app

--- publish-services/auth-service/app.js
+++ publish-services/auth-service/app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authController = require('./controllers/authController');
-// const consumeQueueAndPostToDatabase = require('./consumers/loginqueue');
 
 const app = express();
 const port = 3001;
@@ -10,7 +9,6 @@ const port = 3001;
 app.use(bodyParser.json());
 
 app.post('/login', authController.login);
-// consumeQueueAndPostToDatabase();
 
 app.listen(port, () => {
   console.log(`Auth service is running on port ${port}`);
