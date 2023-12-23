99938ce4c650df7944b788b5ce45d18a4070fbd4
tirth4112 <tdpanchal4112@gmail.com>
2023-12-23 19:46:00 +0530

    initial setup

--- publish-services/auth-service/controllers/Admin_Login_Controller.js
+++ publish-services/auth-service/controllers/Admin_Login_Controller.js
@@ -1,12 +1,12 @@
 const amqp = require('amqplib');
 const queueNames=require('../../../api-gateway/Rabbitmq/Queue.json')
-const publishToQueue =require('../../../api-gateway/SendingToQueue/publishToQueue.cjs').default;
+const  publishToQueue =  require('../../../api-gateway/SendingToQueue/publishToQueue.cjs');
 exports.Admin_Login_Controller = async (req, res) => {
 try
 {  const queues = queueNames.Auth_user;
 
   const data = req.query;
-console.log(data.username);
+
   if(data.username==undefined || data.password==undefined)
   { 
     res.status(500).json({ message: 'Empty Field' });
