032ceea81c8443269ba80d7941e56621befc0540
tirth4112 <tdpanchal4112@gmail.com>
2023-12-23 15:57:43 +0530

    j

--- publish-services/Registration-service/app.js
+++ publish-services/Registration-service/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const amqp = require('amqplib');
 // const consumeQueueAndPostToDatabase = require('./consumers/loginqueue');
-
+sd
 const app = express();
 const port = 3003;
 
