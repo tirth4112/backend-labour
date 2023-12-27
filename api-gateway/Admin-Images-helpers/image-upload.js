31ccfd8e7c309bdfa7dd6eb7f0dcb8ca0566a441
tirth4112 <tdpanchal4112@gmail.com>
2023-12-27 21:52:03 +0530

    add

--- api-gateway/Admin-Images-helpers/image-upload.js
+++ api-gateway/Admin-Images-helpers/image-upload.js
@@ -2,7 +2,7 @@ const multer=require('multer');
 const path=require('path');
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads')
+        cb(null, './Adminuploads')
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname))
--- publish-services/auth-service/app.js
+++ publish-services/auth-service/app.js
@@ -1,21 +1,29 @@
 // cd backend-labour/publish-services/auth-service 
 const express = require('express');
 const bodyParser = require('body-parser');
-// Change the file name from Check_Admin.js to Check_Admin.cjs
-const middleware_Admin = require('../../api-gateway/middleware/Check_Admin.cjs');
-const Auth_Registration=require('./controllers/Admin_Registration_Controller')
-const Auth_Login=require('./controllers/Admin_Login_Controller')
-const Admin_ForgetPassword=require('./controllers/Admin_ForgetPassword')
+
+
+const post=require('./routes/post');
+const get=require('./routes/get');
+
+// // Change the file name from Check_Admin.js to Check_Admin.cjs
+// const middleware_Admin = require('../../api-gateway/middleware/Check_Admin.cjs');
+// const Auth_Registration=require('./controllers/Admin_Registration_Controller')
+// const Auth_Login=require('./controllers/Admin_Login_Controller')
+// const Admin_ForgetPassword=require('./controllers/Admin_ForgetPassword')
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 
-app.post('/Admin-Registration',
-Auth_Registration.Registeration);
+app.use("/post",post);
+app.use("/get",get);
 
-app.post('/Admin-Login', Auth_Login.Admin_Login_Controller);
-app.post('/Admin-ForgetPassword', Admin_ForgetPassword.Admin_ForgetPassword);
+// app.post('/Admin-Registration',
+// Auth_Registration.Registeration);
+
+// app.post('/Admin-Login', Auth_Login.Admin_Login_Controller);
+// app.post('/Admin-ForgetPassword', Admin_ForgetPassword.Admin_ForgetPassword);
 
 app.listen(port, () => {
   console.log(`Auth service is running on port ${port}`);
